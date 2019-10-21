21851d95601876e0c0b58ef2a6a1f5de5f3090ea
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-10-21 22:12:13 +0200

    fix: remove signale usage

--- web-app/tasks/import.js
+++ web-app/tasks/import.js
@@ -1,16 +1,15 @@
 const ncp = require('ncp');
 const {from_current} = require('./misc');
-const signale = require('signale');
 
 module.exports.import_assets = async function() {
 
     return new Promise((ok, ko) => {
         ncp(from_current('./portal/assets'), from_current('./static/assets'), (err) => {
             if (err) {
-                signale.fatal(err);
+                console.error(err);
                 ko(err);
             } else {
-                signale.success('Properly imported assets');
+                console.log('Properly imported assets');
                 ok();
             }
         });
